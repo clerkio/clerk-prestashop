07e0937bd7be21eb8507845172e637f8f2e64b78
Rasmus Bech <me@rbech.com>
2017-05-24 11:49:57 +0200

    Fix pagination for categories

--- controllers/front/category.php
+++ controllers/front/category.php
@@ -12,36 +12,45 @@ class ClerkCategoryModuleFrontController extends ClerkAbstractFrontController
     {
         $response = array();
 
-        $categories = Category::getCategories(Configuration::get('PS_LANG_DEFAULT'), true, false);
+        $limit = sprintf('LIMIT %s', $this->limit);
+
+        if ($this->offset > 0) {
+            $limit .= sprintf(' OFFSET %s', $this->offset);
+        }
+
+        $filter = "AND c.id_category != " . Configuration::get('PS_ROOT_CATEGORY');
+        $categories = Category::getCategories(Configuration::get('PS_LANG_DEFAULT'), true, false, $filter, '', $limit);
 
         $id_lang = Configuration::get('PS_LANG_DEFAULT');
 
-        foreach ($categories as $category) {
-            if ($category['id_category'] === Configuration::get('PS_ROOT_CATEGORY')) {
-                continue;
+        if ($categories) {
+            foreach ($categories as $category) {
+                if ($category['id_category'] === Configuration::get('PS_ROOT_CATEGORY')) {
+                    continue;
+                }
+
+                $item = [
+                    'id' => $category['id_category'],
+                    'name' => $category['name'],
+                    'url' => $this->context->link->getCategoryLink($category['id_category'], null, Configuration::get('PS_LANG_DEFAULT')),
+                ];
+
+                //Append parent id
+                if ($category['id_parent'] !== Configuration::get('PS_ROOT_CATEGORY')) {
+                    $item['parent'] = $category['id_parent'];
+                }
+
+                //Append subcategories
+                $categoryObj = new Category($category['id_category']);
+                $subCategories = $categoryObj->getSubCategories($id_lang);
+
+                $item['subcategories'] = array();
+                foreach ($subCategories as $subCategory) {
+                    $item['subcategories'][] = (int)$subCategory['id_category'];
+                }
+
+                $response[] = $item;
             }
-
-            $item = [
-                'id' => $category['id_category'],
-                'name' => $category['name'],
-                'url' => $this->context->link->getCategoryLink($category['id_category'], null, Configuration::get('PS_LANG_DEFAULT')),
-            ];
-
-            //Append parent id
-            if ($category['id_parent'] !== Configuration::get('PS_ROOT_CATEGORY')) {
-                $item['parent'] = $category['id_parent'];
-            }
-
-            //Append subcategories
-            $categoryObj = new Category($category['id_category']);
-            $subCategories = $categoryObj->getSubCategories($id_lang);
-
-            $item['subcategories'] = array();
-            foreach ($subCategories as $subCategory) {
-                $item['subcategories'][] = (int)$subCategory['id_category'];
-            }
-
-            $response[] = $item;
         }
 
         return $response;
