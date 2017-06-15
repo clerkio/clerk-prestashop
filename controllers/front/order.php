d71e2228588056b0e7ddf5c4257fab52d39bbd5d
Rasmus Bech <me@rbech.com>
2017-06-15 10:48:56 +0200

    Add pagination for orders

--- controllers/front/order.php
+++ controllers/front/order.php
@@ -44,8 +44,17 @@ class ClerkOrderModuleFrontController extends ClerkAbstractFrontController
     public function getJsonResponse()
     {
         $response = array();
+        $limit = '';
 
-        $orders = Order::getOrdersWithInformations();
+        if ($this->limit > 0) {
+            $limit = sprintf('LIMIT %s', $this->limit);
+        }
+
+        if ($this->offset > 0) {
+            $limit .= sprintf(' OFFSET %s', $this->offset);
+        }
+
+        $orders = $this->getOrdersWithInformations($limit);
 
         $fields = array_flip($this->fieldMap);
 
@@ -79,5 +88,27 @@ class ClerkOrderModuleFrontController extends ClerkAbstractFrontController
             'email',
             'customer',
         ];
+    }
+
+    protected function getOrdersWithInformations($limit = null, Context $context = null)
+    {
+        if (!$context) {
+            $context = Context::getContext();
+        }
+
+        $sql = 'SELECT *, (
+					SELECT osl.`name`
+					FROM `'._DB_PREFIX_.'order_state_lang` osl
+					WHERE osl.`id_order_state` = o.`current_state`
+					AND osl.`id_lang` = '.(int)$context->language->id.'
+					LIMIT 1
+				) AS `state_name`, o.`date_add` AS `date_add`, o.`date_upd` AS `date_upd`
+				FROM `'._DB_PREFIX_.'orders` o
+				LEFT JOIN `'._DB_PREFIX_.'customer` c ON (c.`id_customer` = o.`id_customer`)
+				WHERE 1
+					'.Shop::addSqlRestriction(false, 'o').'
+				ORDER BY o.`date_add` DESC
+				'.($limit != '' ? $limit : '');
+        return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
     }
 }
