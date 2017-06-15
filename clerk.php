a1247413e7860e1e9e7fc872a80d31882d0607bd
Rasmus Bech <me@rbech.com>
2017-06-15 15:22:13 +0200

    Bump version

--- clerk.php
+++ clerk.php
@@ -12,7 +12,7 @@ class Clerk extends Module
 	{
 		$this->name = 'clerk';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.4.1';
+		$this->version = '1.4.2';
 		$this->author = 'Clerk';
 		$this->need_instance = 0;
 		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
