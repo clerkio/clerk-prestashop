6b3eecd95da48c0c9b487f8a793603c31d8fc97d
Casper Kromann Nielsen <ckn@clerk.io>
2020-03-02 13:11:09 +0100

    Version 6.0.0

--- clerk.php
+++ clerk.php
@@ -67,7 +67,7 @@ class Clerk extends Module
         $this->api = new Clerk_Api();
         $this->name = 'clerk';
         $this->tab = 'advertising_marketing';
-        $this->version = '5.3.2';
+        $this->version = '6.0.0';
         $this->author = 'Clerk';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
