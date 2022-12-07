6ad11766ba863e4bb83c766d2a8230aad78432e9
Alexander Stage <89036433+FoulBachelor@users.noreply.github.com>
2022-12-07 20:04:52 +0100

    Update pack_module.sh

--- pack_module.sh
+++ pack_module.sh
@@ -4,7 +4,7 @@ mkdir -p $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
 cp -rf $SCRIPT_DIR/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
 mv -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk
 rm -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/pack_module.sh
-zip -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk
 cd $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
+zip -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip clerk
 ls | grep clerk_prestashop.zip
 echo "Module Packed and Ready!"
