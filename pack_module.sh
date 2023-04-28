babca3aee67d61b3a8565da5b85a089039ed52fc
Stubbe <abs@clerk.io>
2023-04-28 11:56:50 +0200

    made pack script self contained

--- pack_module.sh
+++ pack_module.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
+ZIP_FILE=$SCRIPT_DIR/clerk_prestashop.zip
+if [ -f "$ZIP_FILE" ]; then
+    rm -f $ZIP_FILE
+fi
 cp -rf $SCRIPT_DIR/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
 mv -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk
 rm -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/pack_module.sh
 cd $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
 zip -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip clerk
+cp $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip $SCRIPT_DIR
+rm -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
 ls | grep clerk_prestashop.zip
 echo "Module Packed and Ready!"
