35e10e8ecde8dca8a747a741ef5a00a2e2a09a17
Stubbe <abs@clerk.io>
2024-01-10 10:42:46 +0100

    made false comparison specific

--- controllers/front/ClerkAbstractFrontController.php
+++ controllers/front/ClerkAbstractFrontController.php
@@ -457,7 +457,7 @@ abstract class ClerkAbstractFrontController extends ModuleFrontController
      */
     protected function getLanguageId()
     {
-        if (!$this->getLanguages()) {
+        if ($this->getLanguages() === false) {
             return $this->context->language->id;
         } else {
             return $this->getLanguages();
