ef4462cf13d1d412c2dc16090bca0f34febfff6c
Alexander Stage <abs@stubbe.dev>
2024-02-14 10:09:48 +0100

    updated pack script

--- pack_module.sh
+++ pack_module.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-mkdir -p $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
-ZIP_FILE=$SCRIPT_DIR/clerk_prestashop.zip
-if [ -f "$ZIP_FILE" ]; then
-    rm -f $ZIP_FILE
-fi
-cp -rf $SCRIPT_DIR/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
-mv -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/* $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk
-rm -f $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/pack_module.sh
-cd $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
-zip -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip clerk
-cp $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK/clerk_prestashop.zip $SCRIPT_DIR
-rm -r $SCRIPT_DIR/../PRESTASHOP_MODULE_PACK
-ls | grep clerk_prestashop.zip
-echo "Module Packed and Ready!"
+
+# Set the name of the zip file
+zip_file="clerk_prestashop.zip"
+
+# Create a temporary directory
+temp_dir=$(mktemp -d)
+orig_dir=$(realpath $(dirname $0))
+
+rm -rf "./clerk_prestashop.zip"
+
+# Create a parent folder named 'clerk' in the temporary directory and copy the contents into it
+mkdir "$temp_dir/clerk" && cp -r * "$temp_dir/clerk"
+
+cd "$temp_dir"
+
+# Zip the 'clerk' folder
+zip -r "$zip_file" "clerk"
+
+mv "clerk_prestashop.zip" "$orig_dir/clerk_prestashop.zip"
+
+echo "Packed Prestashop Module"
+ls | grep "*.zip"
+
+# Clean up by removing the temporary directory
+rm -rf "$temp_dir"
