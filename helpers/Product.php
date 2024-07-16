7720560736a45b72acb32661dfe8fe0d426d8958
Stubbe <abs@clerk.io>
2024-07-16 15:27:14 +0200

    first draft transliterate cyrrilic attribute keys

--- helpers/Product.php
+++ helpers/Product.php
@@ -644,7 +644,26 @@ class ProductHelper {
         $product_data = ProductHelper::getVariantData($context, $shop_id, $language_id, $product_id, $product, $product_data);
         $product_data = ProductHelper::getChildData($shop_id, $language_id, $product_id, $product_data);
         $product_data = ProductHelper::getTierPrices($shop_id, $product_id, $product_data);
+
+        // Iterate over product data and regex replace all keys to remove cyrillic chars
+
+        $fproduct_data = [];
+        foreach ($product_data as $key => $value) {
+          $fproduct_data[ProductHelper::transliterateKey($key)] = $value;
+        }
+
         return $product_data;
 
-}
+    }
+
+    public static transliterateKey($key) {
+      $cyr  = array('а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у', 
+            'ф','х','ц','ч','ш','щ','ъ', 'ы','ь', 'э', 'ю','я','А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У',
+            'Ф','Х','Ц','Ч','Ш','Щ','Ъ', 'Ы','Ь', 'Э', 'Ю','Я' );
+      $lat = array( 'a','b','v','g','d','e','io','zh','z','i','y','k','l','m','n','o','p','r','s','t','u',
+                  'f' ,'h' ,'ts' ,'ch','sh' ,'sht' ,'a', 'i', 'y', 'e' ,'yu' ,'ya','A','B','V','G','D','E','Zh',
+                  'Z','I','Y','K','L','M','N','O','P','R','S','T','U',
+                  'F' ,'H' ,'Ts' ,'Ch','Sh' ,'Sht' ,'A' ,'Y' ,'Yu' ,'Ya' );
+      return str_replace($cyr, $lat, $key);
+    }
 }
